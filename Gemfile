56498fe8e028830872ff35691ba94cdbf935b84a
Dan Segal <danSegal2@gmail.com>
2017-09-09 19:45:10 -0400

    Make initial cards html using fullpage.js

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'pry'
 gem 'rwordnet'
 gem 'lemmatizer'
 gem 'friendly_id'
+gem 'fullpage-rails'
+gem 'jquery-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4.rc1'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require rails-ujs
+//= require fullpage
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function() {
+    $('#fullpage').fullpage();
+});
