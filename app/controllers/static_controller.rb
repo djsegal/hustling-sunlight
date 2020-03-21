3e58112f5cca109aaed3a71b0902da458d971943
Dan Segal <dansegal2@gmail.com>
2020-03-20 22:39:55 -0700

    Add share route

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,7 @@
 class StaticController < ApplicationController
   def home
   end
+
+  def share
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'static/home'
   root to: 'static#home'
 
+  get '/:phrase', to: "static#share"
+
   resources :phrases
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
